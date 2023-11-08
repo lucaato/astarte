dd67c33b64a664169372fea187dabdb40ec018ba
Luca Arato <luca.arato@secomind.com>
2023-11-08 12:29:04 +0100

    Working shell with buildGoModule

--- flake.nix
+++ flake.nix
@@ -4,25 +4,39 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    astartectl = {
-      type = "github";
-      owner = "astarte-platform";
-      repo = "astartectl";
-      ref = "release-23.5";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, astartectl, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: 
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ import astartectl.overlays.${system}.default ];
         };
+        astartectl =
+          let
+            version = "23.5.0";
+          in
+          pkgs.buildGoModule {
+            inherit version;
+            pname = "astartectl";
+            src = pkgs.fetchFromGitHub {
+              owner = "astarte-platform";
+              repo = "astartectl";
+              rev = "v${version}";
+              sha256 = "sha256-4NgDVuYEeJI5Arq+/+xdyUOBWdCLALM3EKVLSFimJlI=";
+            };
+            vendorSha256 = "sha256-Syod7SUsjiM3cdHPZgjH/3qdsiowa0enyV9DN8k13Ws=";
+            # Completion
+            nativeBuildInputs = with pkgs; [
+              installShellFiles
+            ];
+            postInstall = ''
+              installShellCompletion --cmd astartectl \
+                --bash <($out/bin/astartectl completion bash) \
+                --zsh <($out/bin/astartectl completion zsh) \
+                --fish <($out/bin/astartectl completion fish)
+            '';
+          };
       in
       with pkgs;
       {
